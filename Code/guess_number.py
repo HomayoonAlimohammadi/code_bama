0a06ad6614689862cfe055f9d909a9afac5edd99
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-04-04 17:12:08 +0430

    Add guess_number.py

---
+++ Code/guess_number.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+computer_number = randint(10,100)
+
+for i in range(6):
+
+    user_guess = int(input('Eneter a number from 10 to 100: '))
+
+    if user_guess == computer_number:
+        print('You won!')
+        break
+    
+    elif user_guess < computer_number:
+        print('You guessed too low!')
+
+    else:
+        print('You guessed too high!')
+
+else:
+    print('The number was:', computer_number)
+    print('Maybe next time!')
