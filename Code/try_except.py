c61742eb513741fd394acc6c8f75d3b0b87f911f
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-04-14 08:05:55 +0430

    Update Try Except

--- Code/try_except.py
+++ Code/try_except.py
@@ -1,13 +1,15 @@
 def isPrime(n):
     '''
-    Check too see if a number is Prine
+    Check to see if a number is Prime
     '''
     try:
+        n = float(n)
         for i in range(2, int(n**0.5) + 1):
             if n % i == 0:
                 return False
         return True
-    except (ValueError, TypeError) as e:
+    except ValueError as e:
+        print(e.__class__.__name__)
         print(e)
         return False
 
@@ -15,33 +17,37 @@ def isPrime(n):
         print('isPrime function was called')
     
 
-number = int(input('Enter a number: '))
-
-
-
-try:
-    number = int(input('Enter a number: '))
-
-except:
-    print('You did not enter a number.')
-
-
-
-try:
-    number = int(input('Enter a number: '))
-    result = 10 / number
-
-except (ValueError, ZeroDivisionError) as e:
-    print(e)
-
-
-
 number = input('Enter a number: ')
+print(isPrime(number))
 
-if number.isdigit() and number != 0:
-    number = int(number)
-    result = 10 / number
 
-else:
-    print('Invalid input.')
+# try:
+#     number = int(input('Enter a number: '))
+
+# except:
+#     print('You did not enter a number.')
+
+
+
+
+# try:
+#     number = int(input('Enter a number: '))
+#     result = 10 / number
+
+# except (ValueError, ZeroDivisionError) as e:
+#     print(e)
+
+# finally:
+#     print('Execution was finished.')
+
+
+
+# number = input('Enter a number: ')
+
+# if number.isdigit() and number != 0:
+#     number = int(number)
+#     result = 10 / number
+
+# else:
+#     print('Invalid input.')
 
